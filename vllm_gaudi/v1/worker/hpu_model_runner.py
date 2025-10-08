fd90b3c307be059fbb759706fe886673332f4f90
Kacper Karwot <kkarwot@habana.ai>
2025-10-08 14:01:22 +0300

    missing arg fix

--- vllm_gaudi/v1/worker/hpu_model_runner.py
+++ vllm_gaudi/v1/worker/hpu_model_runner.py
@@ -735,6 +735,7 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
             self.kv_cache_dtype,
             self.block_size,
             use_mla=self.model_config.use_mla,
+            use_sparse=False,
         )
 
         # Mult-modal-related.
