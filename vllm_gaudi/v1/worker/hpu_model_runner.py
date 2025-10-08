95668dcbcbd950d1a323de30eb74dc645f39faf4
Kacper Karwot <kkarwot@habana.ai>
2025-10-08 14:17:48 +0300

    test

--- vllm_gaudi/v1/worker/hpu_model_runner.py
+++ vllm_gaudi/v1/worker/hpu_model_runner.py
@@ -670,6 +670,7 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
         self.observability_config = vllm_config.observability_config
         self.is_driver_worker = is_driver_worker
         self.use_aux_hidden_state_outputs = False
+        self.use_sparse = False
         self.supports_mm_inputs = False
 
         self.sampler = Sampler()
@@ -735,6 +736,7 @@ class HPUModelRunner(KVConnectorModelRunnerMixin):
             self.kv_cache_dtype,
             self.block_size,
             use_mla=self.model_config.use_mla,
+            use_sparse=self.model_config.use_sparse,
         )
 
         # Mult-modal-related.
